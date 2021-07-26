d839e8441179516d371d41cce2927cb3b5018e34
Phoomparin Mano <poom@hey.com>
2021-07-27 00:12:58 +0700

    chore: colors

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <div tw="min-h-screen bg-gray-900 text-white">
       <div tw="max-w-5xl mx-auto py-6">
-        <div tw="bg-purple-500 px-4 py-2 text-center">
+        <div tw="bg-red-500 px-4 py-2 text-center">
           <Title>Hello</Title>
         </div>
       </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,16 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
+    colors: {
+      red: colors.rose,
+      pink: colors.fuchsia,
+      gray: colors.trueGray,
+      white: colors.white,
+    },
   },
   variants: {
     extend: {},
