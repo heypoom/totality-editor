3b7ff165cf72b4b4d3af1d4b360d88c844614a52
Phoomparin Mano <poom@hey.com>
2021-09-15 02:08:03 +0700

    chore: modify panel comments

--- src/@types/LayoutState.ts
+++ src/@types/LayoutState.ts
@@ -3,13 +3,13 @@ export type LayoutPreset =
   | 'horizontal-split'
   | 'visual-in-background'
 
-/** Renderer area allows you to render algorithm visualizations, simulations, previews and more. */
+/** Renderer Panel allows you to render algorithm visualizations, simulations, previews and more. */
 type RendererPanel = {type: 'renderer'; renderer: string}
 
-/** Editor area allows you to display the Monaco editor. */
+/** Editor Panel displays the Monaco editor for code editing. */
 type EditorPanel = {type: 'editor'; uri: string}
 
-/** Controls area allows you to tweak each parameters and interact with the simulation. */
+/** Controls Panel allows you to tweak each parameters and interact with the simulation. */
 type ControlsPanel = {type: 'controls'}
 
 export type Panel = RendererPanel | EditorPanel | ControlsPanel
