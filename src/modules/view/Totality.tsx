e9520ae53d502335a21c239b08f464d20ed6919f
Phoomparin Mano <poom@hey.com>
2021-09-22 21:39:14 +0700

    refactor: document props

--- src/modules/view/Totality.tsx
+++ src/modules/view/Totality.tsx
@@ -11,11 +11,22 @@ import {TotalityErrorBoundary} from 'modules/common/ErrorBoundary'
 import {OptionsOf, Extension} from '@types'
 
 export interface ITotalityProps<E extends readonly Extension<any, any>[]> {
+  /** Extensions to add functionality to the editor. */
   extensions?: E
+
+  /** Options to customize the editor. */
   options?: OptionsOf<E>
+
+  /** Code displayed in the current editor. */
   code?: string
+
+  /** File name of the current editor. */
   path?: string
+
+  /** Persists the editor's content in the local storage. */
   persist?: boolean
+
+  /** Injects these variables into the global runner scope. */
   scope?: Record<string, unknown>
 }
 
