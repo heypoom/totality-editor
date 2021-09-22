36f94585c9078e356a95a089625648502c5658f1
Phoomparin Mano <poom@hey.com>
2021-09-23 01:29:51 +0700

    feat: toggle layout

--- src/pages/glsl-live.tsx
+++ src/pages/glsl-live.tsx
@@ -9,7 +9,7 @@ import {
 } from '@totality/extensions'
 
 import {GLSLSample} from 'constants/sample/glsl.sample'
-import React from 'react'
+import React, {useReducer} from 'react'
 
 const extensions = [
   DraculaThemeExtension,
@@ -18,19 +18,30 @@ const extensions = [
 ] as const
 
 export default function GLSLLiveDemo() {
+  const [isLiveLayout, toggle] = useReducer((s) => !s, true)
+
   return (
-    <div tw="bg-purple-500 text-gray-800 min-h-screen">
+    <div tw="bg-purple-500 text-gray-800 min-h-screen relative">
+      <button
+        tw="absolute z-10 bg-red-500 bottom-5 right-5 px-3 py-1 text-white shadow-lg"
+        onClick={toggle}
+      >
+        toggle layout
+      </button>
+
       <Totality
+        height="100vh"
+        code={GLSLSample}
         path="shader.glsl"
         extensions={extensions}
-        code={GLSLSample}
-        height="100vh"
-        layout="visual-in-background"
-        options={{
-          'theme.background': '#00000000',
-          'theme.highlight': '#00000022',
-          'editor.fontSize': 21,
-        }}
+        {...(isLiveLayout && {
+          layout: 'visual-in-background',
+          options: {
+            'theme.background': '#00000000',
+            'theme.highlight': '#00000022',
+            'editor.fontSize': 21,
+          },
+        })}
       />
     </div>
   )
