27d07502a35d7e1aa60462035bd5143f469650d8
Phoomparin Mano <poom@hey.com>
2021-10-03 11:25:10 +0700

    fix: utility function types for createMerge

--- src/modules/store/utils/merge.ts
+++ src/modules/store/utils/merge.ts
@@ -1,7 +1,10 @@
 import {State} from '@types'
 
+/** Some of the state fields are string, so they're not mergeable. */
+type StateKeys = Exclude<keyof State, 'code'>
+
 export const createMerge =
-  <K extends keyof State>(key: K) =>
+  <K extends StateKeys>(key: K) =>
   (state: State, data: Partial<State[K]>): Partial<State> => ({
     [key]: {...state[key], ...data},
   })
